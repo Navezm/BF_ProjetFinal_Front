aff9b76677f00d5c6c2699116c31b74cf516f20a
Navezm <navez.martin@gmail.com>
2021-11-09 14:24:56 +0100

    Add pictures & creation module/home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/home/home.module')
+      .then(t => t.HomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
