2161ee8c476bb1bc26e0f3e3fefd7bbfffb63731
Navezm <navez.martin@gmail.com>
2021-11-10 11:36:18 +0100

    Contact done

--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactViewComponent } from './contact-view/contact-view.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
   declarations: [
-    ContactViewComponent
+    ContactViewComponent,
+    ContactFormComponent
   ],
   imports: [
     CommonModule,
