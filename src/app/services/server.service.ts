e4dc5ea88e9531db3349e1d8db10255a4a93f9b9
Martin Navez <navez.martin@gmail.com>
2021-11-12 13:48:36 +0100

    ServerService & CRUD interface done

--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+
+const SERVER_URL: string = environment.api.url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
+  private jwt: JwtHelperService = new JwtHelperService();
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public get<T>(url: string): Observable<T> {
+    return this.http.get<T>(SERVER_URL + url);
+  }
+
+  public post<T>(url: string, body: T): Observable<T> {
+    return this.http.post<T>(SERVER_URL + url, body);
+  }
+
+  public put<T>(url: string, body: T): Observable<T> {
+    return this.http.put<T>(SERVER_URL + url, body);
+  }
+
+  public delete<T>(url: string): Observable<T> {
+    return this.http.delete<T>(SERVER_URL + url);
+  }
 }
