2d7fa850820e141c15320bdea5ddfb634b70c65a
Navezm <navez.martin@gmail.com>
2021-11-15 09:22:39 +0100

    Shop done

--- src/app/modules/shop/basket/basket.component.ts
+++ src/app/modules/shop/basket/basket.component.ts
@@ -7,8 +7,7 @@ import {PaintingPurchaseService} from "../../../services/painting-purchase.servi
 import {PaintingPurchase} from "../../../models/paintingPurchase.model";
 import {Status} from "../../../models/enums/status.enum";
 import {User} from "../../../models/user.model";
-import {UserService} from "../../../services/user.service";
-import {Role} from "../../../models/role.model";
+import {PicturePurchase} from "../../../models/picturePurchase.model";
 
 @Component({
   selector: 'app-basket',
@@ -21,8 +20,7 @@ export class BasketComponent implements OnInit {
   constructor(
     private router: Router,
     private picturePurchaseService: PicturePurchaseService,
-    private paintingPurchaseService: PaintingPurchaseService,
-    private userService: UserService
+    private paintingPurchaseService: PaintingPurchaseService
   ) { }
 
   ngOnInit(): void {
@@ -39,7 +37,6 @@ export class BasketComponent implements OnInit {
   }
 
   sendPurchase(items: any[]){
-    console.log(items);
     if(this.router.url.includes("painting")){
       this.sendPaintingPurchase(items);
     } else {
@@ -49,15 +46,6 @@ export class BasketComponent implements OnInit {
 
   sendPaintingPurchase(items: Painting[]){
     let user: User = JSON.parse(<string>localStorage.getItem('user')) as User;
-    // let user: User;
-    // console.log(localUser)
-    // if (localUser.id != null) {
-    //   this.userService.getOneById(localUser.id).subscribe((userOptional: User) => user = userOptional);
-    // } else {
-    //   user = localUser
-    // }
-
-    console.log(user);
 
     let panier: PaintingPurchase = {
       status: Status.PENDING,
@@ -65,13 +53,20 @@ export class BasketComponent implements OnInit {
       user: user,
       address: user.address
     }
-    console.log(panier);
 
     this.paintingPurchaseService.insert(panier).subscribe((data: PaintingPurchase) => console.log(data));
   }
 
   sendPicturePurchase(items: Picture[]){
+    let user: User = JSON.parse(<string>localStorage.getItem('user')) as User;
 
+    let panier: PicturePurchase = {
+      status: Status.PENDING,
+      pictures: items,
+      user: user
+    }
+
+    this.picturePurchaseService.insert(panier).subscribe((data: PicturePurchase) => console.log(data));
   }
 
 }
