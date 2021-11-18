65020d7a1beffce8e20733fed6c0a14798135dba
Navezm <navez.martin@gmail.com>
2021-11-18 15:40:20 +0100

    styles changes

--- src/app/modules/admin/admin/user-list/user-list.component.ts
+++ src/app/modules/admin/admin/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../../../services/user.service";
 import {User} from "../../../../models/user.model";
+import {UtilService} from "../../../../services/util.service";
 
 @Component({
   selector: 'app-user-list',
@@ -11,12 +12,18 @@ export class UserListComponent implements OnInit {
   userList!: User[];
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private utilService: UtilService
   ) { }
 
   ngOnInit(): void {
     this.userService.getAll()
       .subscribe((data) => this.userList = data);
+  }
+
+  delete(id: any){
+    this.userService.delete(id).subscribe();
+    this.utilService.reloadComponent();
   }
 
 }
