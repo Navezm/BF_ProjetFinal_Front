f0d997ec1a263fcffbcdae2eaf6207be1d47297c
Martin Navez <navez.martin@gmail.com>
2021-11-22 21:08:40 +0100

    Email sender done

--- src/app/modules/contact/contact-form/contact-form.component.ts
+++ src/app/modules/contact/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormGroup} from "@angular/forms";
+import {F_Email} from "./email.form";
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +9,27 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
+  emailFom: FormGroup = new FormGroup(F_Email);
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    const values = this.emailFom.value;
+    this.http.post("http://localhost:8080/sendMail", values).subscribe();
+    this.emailSent();
+  }
+
+  emailSent(){
+    let divOrder = document.getElementById('divOrderDone');
+    // @ts-ignore
+    divOrder.style.display = 'block';
+
+    this.emailFom.reset();
   }
 
 }
